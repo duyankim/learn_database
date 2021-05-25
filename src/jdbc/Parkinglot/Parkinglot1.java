8b6a7bf5699adccdc2cf901b0a1a670387144278
duyankim <arbolkim@gmail.com>
2021-05-26 00:00:13 +0900

    공영주차장 데이터 가공하고 insert하기

--- src/jdbc/Parkinglot/Parkinglot1.java
+++ src/jdbc/Parkinglot/Parkinglot1.java
@@ -1,5 +1,10 @@
 package jdbc.Parkinglot;
 
+/* 2021-05-24
+ * 데이터베이스 프로그래밍 2강 - 주차장 데이터
+ * kopo03 김도연
+ */
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -13,18 +18,18 @@ public class Parkinglot1 {
 				"jdbc:mysql://192.168.23.27:3306/kopoctc", "root", "kopoctc");
 		Statement stmt = conn.createStatement();								// Statement 객체는 SQL문을 데이터베이스로 전송하는데 사용한다.
 
-		stmt.execute("create table public_parking_lot(" 						// 테이블명 freewifi 테이블 생성
-				+ "lot_id int not null primary key, " 					// 설치 장소명
-				+ "lot_name varchar(300), "										// 설치 장소상세
-				+ "longitude double, " 											// 설치 시도명
-				+ "latitude double, " 											// 설치 시군구명
-				+ "lot_acc_type varchar(50), "									// 설치 시설 구분
-				+ "lot_type varchar(50), " 										// 서비스 제공사명
-				+ "lot_addr_land varchar(300), " 								// 와이파이 SSID
-				+ "lot_addr_road varchar(300), "								// 설치 년월
-				+ "lot_cnt varchar(50), " 										// 소재지 도로명주소	
-				+ "fee varchar(50), " 											// 경도 (더블형)
-				+ "region_id int)"										// 데이터 기준일자 (date)
+		stmt.execute("create table public_parking_lot(" 						// 테이블명 public_parking_log 테이블 생성
+				+ "lot_id int not null primary key, " 							// 주차장 관리번호
+				+ "lot_name varchar(300), "										// 주차장 이름
+				+ "longitude double, " 											// 경도
+				+ "latitude double, " 											// 위도
+				+ "lot_acc_type varchar(50), "									// 주차장 구분(공영, 민영 등)
+				+ "lot_type varchar(50), " 										// 주차장 유형(노상, 노외 등)
+				+ "lot_addr_land varchar(300), " 								// 주차장 지번주소
+				+ "lot_addr_road varchar(300), "								// 주차장 도로명주소
+				+ "lot_cnt varchar(50), " 										// 주차 구획수	
+				+ "fee varchar(50), " 											// 요금 (무료, 혼합, 금액 등)
+				+ "region_id int)"												// 지역 코드
 				+ "DEFAULT CHARSET=utf8;");										// utf-8로 저장
 		stmt.close();															// Statement 인스턴스 닫음
 		conn.close();															// Connection 인스턴스 닫음
--- src/jdbc/Parkinglot/Parkinglot2.java
+++ src/jdbc/Parkinglot/Parkinglot2.java
@@ -1,5 +1,10 @@
 package jdbc.Parkinglot;
 
+/* 2021-05-24
+ * 데이터베이스 프로그래밍 2강 - 주차장 데이터
+ * kopo03 김도연
+ */
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -14,14 +19,13 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-
 public class Parkinglot2 {
 
 	public static void main(String[] args) throws ClassNotFoundException, IOException, ParseException, SQLException {
-		Class.forName("com.mysql.cj.jdbc.Driver");								// JDBC 드라이버를 로드한다.
-		Connection conn = DriverManager.getConnection(							// 드라이버를 통해서 데이터베이스와 연결한다.
+		Class.forName("com.mysql.cj.jdbc.Driver");										// JDBC 드라이버를 로드한다.
+		Connection conn = DriverManager.getConnection(									// 드라이버를 통해서 데이터베이스와 연결한다.
 				"jdbc:mysql://192.168.23.27:3306/kopoctc", "root", "kopoctc");
-		Statement stmt = conn.createStatement();								// Statement 객체는 SQL문을 데이터베이스로 전송하는데 사용한다.
+		Statement stmt = conn.createStatement();										// Statement 객체는 SQL문을 데이터베이스로 전송하는데 사용한다.
 		
 		File f = new File("C:\\Users\\kim\\Desktop\\과제\\데이터베이스\\한국교통안전공단_전국공영주차장정보_20191224-3.csv");	// 데이터 파일을 불러온다.
 		BufferedReader br = new BufferedReader(new FileReader(f));						// 버퍼 리더인스턴스를 생성한다.
@@ -34,47 +38,44 @@ public class Parkinglot2 {
 		String[] field_name=readtxt.split(",");											// 필드명을 배열로 받는다.
 		
 		int LineCnt = 0;																// line 수를 세는 변수를 선언한다.
-		while((readtxt = br.readLine()) != null) {										// 
-			String[] field = readtxt.replace("\"","").replace("'","").split(",");
-			String QueryTxt = null;
-			String[] fieldData = {field[0], field[1], field[2], field[3], field[4], field[5], field[6], field[7], field[8], field[16], field[22]};
+		while((readtxt = br.readLine()) != null) {										// 파일의 1줄씩 읽어들인다. 
+			String[] field = readtxt.replace("\"","").replace("'","").split(",");		// 콤마로 구분된 파일을 각각의 필드로 나누어 배열로 만든다.
+			String QueryTxt = null;														// 쿼리 텍스트를 넣을 String 변수를 선언해준다.
+			String[] fieldData = {field[0], field[1], field[2], field[3], field[4], 	// 필요한 필드의 데이터만 넣은 배열을 만들었다.
+					field[5], field[6], field[7], field[8], field[16], field[22]};
 				
-			try {
-				if (field[3] != null) {
-					QueryTxt = makeQueryTxt(fieldData);
+			try {																		// try-catch 문으로 예외 처리를 해준다.
+				if (field[3] != null) {													// 필드 값 중간에 들어간 개행문자로 인해서
+					QueryTxt = makeQueryTxt(fieldData);									// makeQueryTxt는 쿼리문을 만드는 함수다.
 				}
-			} catch (ArrayIndexOutOfBoundsException e) {
-				readtxt = readtxt + br.readLine();
-				field = readtxt.split(",");
-				QueryTxt = makeQueryTxt(fieldData);
-				LineCnt -= 1;
-			} catch (Exception e) {
-				e.printStackTrace();
+			} catch (ArrayIndexOutOfBoundsException e) {								// 만약 배열의 인덱스가 모자랄 경우 예외처리를 한다.
+				readtxt = readtxt + br.readLine();										// 필드 속의 , 때문에 한 줄이 통째로 읽히지 않은 경우로,
+				field = readtxt.split(",");												// 바로 다음 라인을 읽어들여서 문제의 라인에 붙여준다.
+				QueryTxt = makeQueryTxt(fieldData);										// 새로 만든 배열을 makeQueryTxt 함수에 인자로 전달한다.
+				LineCnt -= 1;															// 라인이 중복 카운트되지 않도록 한다.
+			} catch (Exception e) {														// 그 외의 모든 예외는 Exception이 받는다.
+				e.printStackTrace();													// 예외가 발생할 경우 예외를 콘솔에 출력한다.
 			}
-			stmt.execute(QueryTxt);
-			System.out.printf("%d번째 항목 Insert OK \n[%s]\n", LineCnt, QueryTxt);
-			
-			LineCnt++;
+			stmt.execute(QueryTxt);														// 만들어진 쿼리문을 실행한다.
+			System.out.printf("%d번째 항목 Insert OK \n[%s]\n", LineCnt, QueryTxt);		// 실행이 완료되면 프린트한다.
+			LineCnt++;																	// 라인을 1씩 증가시킨다.
 		}
-		br.close();
-
-		stmt.close();
-		conn.close();
+		br.close();																		// BufferedReader 인스턴스를 닫는다.
+		stmt.close();																	// Statement 인스턴스를 닫는다.
+		conn.close();																	// Connection 인스턴스를 닫는다.
 	}	
 
 	static String makeQueryTxt (String[] field) throws ParseException {
-		List<String> field_name = new ArrayList<>(
-				Arrays.asList("lot_id", "lot_name", "longitude", "latitude", "lot_acc_type"
+		List<String> field_name = new ArrayList<>(										// 필드 이름을 넣은 ArrayList를 만들었다.
+				Arrays.asList("lot_id", "lot_name", "longitude", "latitude", "lot_acc_type"	// 가변 배열이기 때문에 Array를 사용하지 않았다.
 				,"lot_type", "lot_addr_land", "lot_addr_road", "lot_cnt", "fee", "region_id"));
 		
-		StringBuffer values = new StringBuffer();		
-		String fields;
-		String QueryTxt = null;
-		List<Integer> removeIdx = new ArrayList<>();
-		
-		try {
-			for (int idx = 0; idx < 11; idx++) {
-				if (field[idx] == null || field[idx].isBlank()) {
+		StringBuffer values = new StringBuffer();										// 가변 문자열일 경우 String보다 StringBuffer가 좋다.																// 필드 
+		String QueryTxt = null;															// 쿼리문을 넣을 문자열을 선언한다.
+		List<Integer> removeIdx = new ArrayList<>();									// 삭제할 필드의 인덱스를 저장하는 ArrayList다.
+		try {																			// null값을 넣으려면 insert할 때 해당 null값의 필드를 빼고 insert하면 된다.
+			for (int idx = 0; idx < 11; idx++) {										// 따라서 빈칸이거나 데이터가 올바르지 않은 필드를 제외시키기 위해서
+				if (field[idx] == null || field[idx].isBlank()) {						//
 					removeIdx.add(idx);
 				} else {
 					if (idx == 0 || idx == 2 || idx == 3 || idx == 10 && field[idx] != null) {
@@ -92,7 +93,7 @@ public class Parkinglot2 {
 			field_name.remove(i);
 		}
 		
-		fields = "insert into public_parking_lot(" + String.join(", ", field_name) + ") values(";
+		String fields = "insert into public_parking_lot(" + String.join(", ", field_name) + ") values(";
 		QueryTxt = fields + values.substring(0, values.length()-2) + ");";
 		return QueryTxt;
 	}
